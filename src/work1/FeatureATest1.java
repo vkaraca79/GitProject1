e3608c4f1f0e8acade45964fa26cc7930409fa39
Volkan K <vkaraca79@hotmail.com>
2021-09-11 22:55:30 +0300

    editing some papers

--- src/work1/FeatureATest1.java
+++ src/work1/FeatureATest1.java
@@ -2,7 +2,10 @@ package work1;
 
 public class FeatureATest1 {
     public static void main(String[] args) {
+
         System.out.println("Doing some work");
+
+        System.out.println("Editing some papers");
     }
 
 }
